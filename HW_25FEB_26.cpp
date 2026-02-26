dffc30ae7424d391adfdf98c2f19ce9f161d8a50
ronniiitt012 <paulronit898@gmail.com>
2026-02-26 23:55:15 +0530

    Add matrix multiplication for 2x2 matrices
    Implement matrix multiplication for 2x2 matrices and output the result.

---
+++ HW_25FEB_26.cpp
@@ -0,0 +1,22 @@
+// multiplication of matrix 2X2
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int A[2][2]={{1,2},{3,4}};
+  int B[2][2]={{5,6},{7,8}};
+  int C[2][2];
+  for(int i=0;i<2;i++){
+    for(int j=0;j<2;j++){
+      C[i][j]=0;
+      for(int k=0;k<2;k++){
+        C[i][j]+=A[i][k]*B[k][j];
+      }
+    }
+  }
+  for(int i=0;i<2;i++){
+    for(int j=0;j<2;j++){
+      cout<<C[i][j]<<" ";
+    }
+  }
+  return 0;
+}
